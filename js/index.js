2581ee951b5698f2aa8d0c29849280520ac1699b
KaustubhBadvegit config --global user.name KaustubhBadvegit config --global user.name KaustubhBadvegit config --global user.name KaustubhBadve <kaustubhbadve7@gmail.com>
2022-04-03 12:57:53 +0530

    hhh

--- js/index.js
+++ js/index.js
@@ -45,6 +45,18 @@ var modal = document.getElementById("myModal");
     }
 
     document.querySelector("#loginn").addEventListener("click" , myfunbecome)
+    var namelogin=JSON.parse(localStorage.getItem("currentUser"))
+
+    namelogin.map(function (elem){
+      var name1=elem.name
+
+      var loginsignup=document.querySelector("#loginn")
+
+      if(name1!="")
+      {
+        loginsignup.innerText=name1
+      }
+    })
 
     function myfunbecome () {
       window.location.href ="login.html"
